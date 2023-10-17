38cc9a025733f7266f26ccc42b527467d46588ed
Shreya-Deokar <deokarshreya14@gmail.com>
2023-10-17 12:16:30 +0530

    my second shell program

--- copyFileCLA.sh
+++ copyFileCLA.sh
@@ -5,8 +5,3 @@ echo "Destination: " $2
 
 cp $1 $2
 
-<<mycomment
-diot@diot-pc:~/EmbeddedLinux$ ./copyFileCLA.sh /home/diot/Desktop/demo.txt /home/diot
-Source File:  /home/diot/Desktop/demo.txt
-Destination:  /home/diot
-mycomment
